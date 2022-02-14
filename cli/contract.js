089c08ba734657979ff7a814af769585a9c83fa9
Alexander Günsche <git@lxg.de>
2022-02-14 11:05:34 +0100

    load URL directly from manifest validator

--- cli/contract.js
+++ cli/contract.js
@@ -4,8 +4,6 @@ import * as fs from 'fs'
 import path from 'path'
 import fetch from 'node-fetch'
 import { URL } from 'url'
-
-import processManifest from '../lib/manifest.js'
 import checkContract from '../lib/contract.js'
 
 ;(async () => {
@@ -14,16 +12,13 @@ import checkContract from '../lib/contract.js'
     if (!url)
         throw new Error("A URL must be provided.")
 
-    const data = await fetch(url).then(x => x.text())
-
     try {
-        const manifest = processManifest(data, url)
         const histFile = `${process.env.npm_config_local_prefix}/.contracts`
         const history = fs.existsSync(histFile)
             ? JSON.parse(fs.readFileSync(histFile))
             : {}
 
-        const newHistory = checkContract(url, manifest, history)
+        const newHistory = checkContract(url, history)
         fs.writeFileSync(histFile, JSON.stringify(newHistory, null, 4))
     } catch (e) {
         console.error(e.message)
