7badfedf6a8cff1830b8794625e46d67af2898e3
Alexander Günsche <alexander.guensche@tui.com>
2022-02-14 11:05:34 +0100

    load URL directly from manifest validator

--- cli/validate.js
+++ cli/validate.js
@@ -4,20 +4,16 @@ import * as fs from 'fs'
 import path from 'path'
 import fetch from 'node-fetch'
 import { URL } from 'url'
-
-// checks
-import processManifest from '../src/manifest.js'
+import getManifest from '../src/manifest.js'
 
 ;(async () => {
-    const manifestUrl = process.argv[2]
+    const url = process.argv[2]
 
-    if (!manifestUrl)
+    if (!url)
         throw new Error("A URL must be provided.")
 
-    const manifest = await fetch(manifestUrl).then(data => data.text())
-
     try {
-        processManifest(manifest)
+        await getManifest(url)
     } catch (e) {
         console.error(e.message)
         process.exit(1)
